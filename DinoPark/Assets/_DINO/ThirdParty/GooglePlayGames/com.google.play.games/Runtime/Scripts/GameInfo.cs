eaf712f0202a0e8be0ae7affd0ab812ad4052352
Bilal Ghareeb <bilal.ghareeb@gmail.com>
2025-08-16 20:22:55 +0300

    Setup GPGS

--- DinoPark/Assets/_DINO/ThirdParty/GooglePlayGames/com.google.play.games/Runtime/Scripts/GameInfo.cs
+++ DinoPark/Assets/_DINO/ThirdParty/GooglePlayGames/com.google.play.games/Runtime/Scripts/GameInfo.cs
@@ -36,9 +36,9 @@ namespace GooglePlayGames {
         private const string UnescapedWebClientId = "WEB_CLIENTID";
         private const string UnescapedNearbyServiceId = "NEARBY_SERVICE_ID";
 
-        public const string ApplicationId = ""; // Filled in automatically
+        public const string ApplicationId = "856244024436"; // Filled in automatically
         public const string IosClientId = "__IOS_CLIENTID__"; // Filled in automatically
-        public const string WebClientId = ""; // Filled in automatically
+        public const string WebClientId = "856244024436-bog73d98lad6k4k1qnthsll0ateo1pb2.apps.googleusercontent.com"; // Filled in automatically
         public const string NearbyConnectionServiceId = "";
 
         public static bool ApplicationIdInitialized() {
