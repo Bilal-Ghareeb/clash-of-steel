018d70096bb684584524a4d285405aa9aa5a59be
Bilal Ghareeb <bilal.ghareeb@gmail.com>
2025-10-16 18:31:52 +0300

    refactor : added new fields to WeaponProgressionData to align with JSON from backend

--- Assets/_COS/Scripts/Backend/Models/WeaponProgressionData.cs
+++ Assets/_COS/Scripts/Backend/Models/WeaponProgressionData.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 public class WeaponProgressionData
 {
     public int damagePerLevel { get; set; }
@@ -6,4 +8,7 @@ public class WeaponProgressionData
     public float costMultiplier { get; set; }
     public int maxLevel { get; set; }
     public string currencyId { get; set; }
+    public int cooldownBaseSeconds { get; set; }
+    public double cooldownMultiplierPerLevel { get; set; }
+    public Dictionary<string, double> cooldownMultiplierPerRarity { get; set; }
 }
